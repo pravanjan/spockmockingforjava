9ae5e7ad6568c1cdea1618a44a37b989f3c0e98a
pravanjan <pravanjan.niranjan.anywhere.co>
2019-02-02 17:51:58 +0530

    added unix line break

--- src/main/java/com/spock/Item.java
+++ src/main/java/com/spock/Item.java
@@ -1,1 +1,4 @@
-package com.spock;public class Item {}
+package com.spock;
+
+public class Item {
+}
--- src/main/java/com/spock/PaymentMethod.java
+++ src/main/java/com/spock/PaymentMethod.java
@@ -1,1 +1,11 @@
-package com.spock;import java.util.List;public class PaymentMethod {    public Boolean processPayment(List<Item> itemList) {        System.out.println("comming inside this method...");        return true;    }}
+package com.spock;
+
+import java.util.List;
+
+public class PaymentMethod {
+
+    public Boolean processPayment(List<Item> itemList) {
+        System.out.println("comming inside this method...");
+        return true;
+    }
+}
